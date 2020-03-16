53b1cd7e634eee5e502d1aa1687c7b55432161e6
Mudassar Nazir <mudassirnazir.1@gmail.com>
2020-03-17 00:23:36 +0500

    index

--- index.js
+++ index.js
@@ -6,9 +6,5 @@ fs.readFile('./index.html', function (err, html) {
     if (err) {
         throw err; 
     }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(8000);
+     
 });
